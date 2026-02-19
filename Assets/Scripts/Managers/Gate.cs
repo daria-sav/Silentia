18c9c1a0a6a25acc93b912179db170553130086d
Daria Savtšenko <daria.savtsenko@gmail.com>
2026-02-19 22:32:21 +0200

    fix: ensure gates trigger only from Player

--- Assets/Scripts/Managers/Gate.cs
+++ Assets/Scripts/Managers/Gate.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Gate : MonoBehaviour
 {
     [SerializeField] private string levelToLoad;
     public SpawnData spawnDataForOtherLevel;
 
+    private Collider2D gateCollider;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        BodyMarkers markers = collision.GetComponentInParent<BodyMarkers>();
+
+        if (markers == null)
+        {
+                Debug.LogWarning("Gate: No Player component found in parent of collider: " + collision.name);
+                return;
+        }
+
+        if (markers.CompareTag("Player"))
         {
             spawnDataForOtherLevel.sceneName = levelToLoad;
 
             SaveLoadManager.instance.Save(spawnDataForOtherLevel, SaveLoadManager.instance.folderName, SaveLoadManager.instance.fileName);
 
-            Player player = collision.GetComponent<Player>();
+            Player player = collision.GetComponentInParent<Player>();
             player.gatherInput.DisablePlayerMap();
             player.physicsControl.ResetVelocity();
             LevelManager.instance.LoadLevelString(levelToLoad);
