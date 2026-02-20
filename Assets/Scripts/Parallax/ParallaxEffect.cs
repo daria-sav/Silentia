7c0079f7efbd2e3e7822e14f0ff3ecd9f8324989
Daria Savtšenko <daria.savtsenko@gmail.com>
2026-02-20 17:16:30 +0200

    fix: lock camera Y and stabilize parallax background

--- Assets/Scripts/Parallax/ParallaxEffect.cs
+++ Assets/Scripts/Parallax/ParallaxEffect.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class ParallaxEffect : MonoBehaviour
 {
+    [Header("Parallax")]
     [SerializeField] private float XparallaxValue;
     [SerializeField] private float YparallaxValue;
+
+    [Header("Pixel snap")]
+    [SerializeField] private int pixelsPerUnit = 16;   
+    [SerializeField] private bool snapToPixelGrid = true;
+
+    [Header("Looping")]
+    [SerializeField] private bool loopX = true;
+
     private float spriteLenght;
     private Camera camera;
     private Vector3 deltaMovement;
@@ -12,22 +21,37 @@ public class ParallaxEffect : MonoBehaviour
     {
         camera = Camera.main;
         lastCameraPosition = camera.transform.position;
+
         spriteLenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void LateUpdate()
     {
-        deltaMovement = camera.transform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * XparallaxValue, deltaMovement.y*YparallaxValue);
-        lastCameraPosition = camera.transform.position;
+        Vector3 camPos = camera.transform.position;
+        Vector3 delta = camPos - lastCameraPosition;
 
-        if (camera.transform.position.x - transform.position.x >= spriteLenght)
-        {
-            transform.position = new Vector3(camera.transform.position.x + spriteLenght, transform.position.y);
-        }
-        else if (transform.position.x - camera.transform.position.x >= spriteLenght)
-        {
-            transform.position = new Vector3(camera.transform.position.x - spriteLenght, transform.position.y);
-        }
+        Vector3 newPos = transform.position + new Vector3(delta.x * XparallaxValue, delta.y * YparallaxValue, 0f);
+
+        newPos.z = transform.position.z;
+
+        if (snapToPixelGrid)
+            newPos = Snap(newPos, pixelsPerUnit);
+
+        transform.position = newPos;
+        lastCameraPosition = camPos;
+
+        if (!loopX) return;
+
+        float diffX = camPos.x - transform.position.x;
+        if (diffX > spriteLenght) transform.position += new Vector3(spriteLenght, 0f, 0f);
+        else if (diffX < -spriteLenght) transform.position -= new Vector3(spriteLenght, 0f, 0f);
+    }
+
+    private Vector3 Snap(Vector3 pos, int ppu)
+    {
+        float unit = 1f / ppu;
+        pos.x = Mathf.Round(pos.x / unit) * unit;
+        pos.y = Mathf.Round(pos.y / unit) * unit;
+        return pos;
     }
 }
